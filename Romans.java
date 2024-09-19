c4763a20059fb68403f6a30d243441ab6aa8910b
smenon345 <smenon@cherrycreekschools.org>
2024-09-19 22:34:52 +0000

    finished code

--- Romans.java
+++ Romans.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Siddharth Menon
 
 import java.util.*;
 
@@ -11,12 +11,26 @@ public class Romans
 	public Romans()
 	{
 		m = new TreeMap<String, Integer>();
+
+		m.put("I", 1);
+		m.put("V", 5);
+		m.put("X", 10);
+		m.put("L", 50);
+		m.put("C", 100);
+		m.put("D", 500);
+		m.put("M", 1000);
 		
 		//complete your map				
 	}
 	
 	public int getNumber( String s )
 	{
+		int value = 0;
+		if (m.containsKey(s)) {
+			value = m.get(s);
+		}
+
+		return value;
 		//complete this method
 	}
 
